f6909fd91fc68ebed61e2999bb05b84aff788938
Mohammed Baba <mbfmz20@gmail.com>
2025-02-22 14:08:07 +0100

    add a sidebar for move between pages

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,9 @@
-// Sidebar.jsx
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="w-64 bg-gray-800 text-white h-screen p-4">
-      <h2 className="text-2xl font-bold mb-6 text-blue-400">Gestion des Stock</h2>
+      <h2 className="text-2xl font-bold mb-6 text-blue-700">Gestion des Stock</h2>
       <ul className="space-y-4">
         <li>
           <Link
